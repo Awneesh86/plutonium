0d7a62c486071aec7bd91dad17113a62d3154cc4
Awneesh Kumar <awneeshkumar860172@gmail.com>
2022-08-07 13:10:25 +0530

    module change

--- src/validator/formatter.js
+++ src/validator/formatter.js
@@ -1,19 +1,19 @@
 function lowercase(){
-    let name1 = "AWNEESH KUMAR"
+    let name1 = "FUNCTIONUP"
     let fullName1 = name1.toLowerCase();
     console.log(fullName1)
     uppercase()
 }
 
 function uppercase(){
-    let name2= "awneesh kumar"
+    let name2= "functionup"
     let fullName2 = name2.toUpperCase();
     console.log(fullName2)
     trim()
 }
 
 function trim(){
-    let name="awneesh kumar"
+    let name="    FunctionUp    "
     let fullName=name.trim();
     console.log(fullName)
 }

83252c594648966dfdc0d5e26025d3c617f0abc3
Awneesh <awneeshkumar860172@gmail.com>
2022-08-10 22:03:46 +0530

    assignment-3

--- src/routes/route.js
+++ src/routes/route.js
@@ -10,6 +10,116 @@ router.get('/test-me', function (req, res) {
     res.send('My second ever api!')
 });
 
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+
+       //LOGIC WILL COME HERE
+       let newPlayer = req.body
+       let newPlayerName = newPlayer.name
+       let isNameRepeated = false
+
+       for(let i = 0; i < players.length; i++){
+        if(players[i].name == newPlayerName){
+            isNameRepeated = true;
+            break
+        }
+       }
+
+       if(isNameRepeated){
+        res.send("This Player was already added!")
+       } else{
+        players.push(newPlayer)
+        res.send(players)
+       }
+
+       res.send(  { data: players , status: true }  )
+   });
+   
+
+router.get("/test-api", function(req,res){
+    res.send({a:56 , b:45})  // JSON CODE IN OBJECT
+});
+router.post("/test-post", function(req,res){
+    res.send([23, 45, 6])
+});
+ 
+// router.post("/test-post-1", function(req,res){
+//     res.send(msg:"Hi", value:true)
+// });
+ 
+
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+//logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+router.get("/sol1", function (req, res) {
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i]; // total = total + arr[i]  // 1+2+3+5+6+7 = 24
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2     // 7*(7+1)/2 = 28
+    let missingNumber= consecutiveSum - total        //    28 - 24 = 4
+  
+    res.send(  { data: missingNumber  }  );  // missing number = 4
+  });
+
+  // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+  //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive   numbers.. n would be length+1 as 1 number is missing
+
+  router.get("/sol2", function (req, res) {
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length    // len = 5
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];  // total = total + arr[i]  // 33+34+35+37+38 = 177
+    }
+  
+    let firstDigit= arr[0]  // 33
+    let lastDigit= arr.pop()  // 38
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2  // (5+1)*(33+38)/2 = 6*71/2 = 426/2 = 213
+    let missingNumber= consecutiveSum - total  // 213 - 177 = 36
+   
+    res.send(  { data: missingNumber  }  );  // missing number = 36
+  });
+  
+ 
+ 
+ 
+
 router.get('/students', function (req, res) {
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
@@ -97,3 +207,4 @@ router.get("/films/:filmId", function (req, res) {
 })
 
 module.exports = router;
+                         // Git Hub Link : -      https://github.com/Awneesh86/plutonium.git

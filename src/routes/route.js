35a83089b3dec34a325030ce77a87d42d97db67d
Awneesh <awneeshkumar860172@gmail.com>
2022-08-29 20:31:04 +0530

    solution/JWT-authencation-1

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const auth = require("../middlewares/auth")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+router.post("/createUsers", userController.createUser  )
 
-router.post("/login", userController.loginUser)
+router.post("/loginUser", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+router.get("/getUsers/:userId", auth.validateToken, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+router.put("/updateUsers/:userId", auth.validateToken, userController.updateUser)
+
+router.delete("/deleteUsers/:userId", auth.validateToken, userController.deleteUser)
 
 module.exports = router;

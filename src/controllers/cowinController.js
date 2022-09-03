0760a4f6ef01f8ed11626768de1852a2b67c8229
Awneesh <awneeshkumar860172@gmail.com>
2022-09-03 11:44:28 +0530

    solution/axios-promises

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,42 +1,75 @@
+
 let axios = require("axios")
 
+//============================================================= Question-1 =====================================================
+/* WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+          Using this Link : --       https://apisetu.gov.in/api/cowin/cowin-public-v2 */
 
-let getStates = async function (req, res) {
-
+let getDistrictSessions = async function (req, res) {
     try {
-        let options = {
-            method: 'get',
-            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
-
-let getDistricts = async function (req, res) {
-    try {
-        let id = req.params.stateId
+        let district = req.query.district_Id
+        let date = req.query.date
         let options = {
             method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district}&date=${date}`
+            
         }
         let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
+        console.log(result.data)
+        // let data = result.data
+        res.status(200).send({ msg: result.data })
     }
     catch (err) {
         console.log(err)
         res.status(500).send({ msg: err.message })
     }
 }
+
+
+//============================================== Question-2 ======================================================================
+
+let getSortedCities = async function (req, res) {
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        let cityObjArray = []
+        for (i = 0; i < cities.length; i++) {
+            let obj = { city: cities[i] }
+            let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=c5a07a5f1ba8b10f0e5cb28b018f097a`)
+            // console.log(resp.data.main.temp)
+            obj.temp = resp.data.main.temp // {city: "Bengaluru", temp: 301.2}
+            cityObjArray.push(obj)
+        }
+        let sorted = cityObjArray.sort(function (a, b) { return a.temp - b.temp })
+        console.log(sorted)
+        res.status(200).send({ status: true, data: sorted })
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ status: false, msg: "server error" })
+    }
+}
+
+//================================================= Question-3 ====================================================//
+
+let memeHandler = async function (req, res) {
+    try {
+        let options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?template_id=181913649&text0=Awneesh Kumar&text1=FunctionUp&username=chewie12345&password=meme@123`
+        }
+        let result = await axios(options)
+        res.send({ data: result.data })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ status: false, msg: "server error" })
+    }
+}
+
+
+
+
+
 
 let getByPin = async function (req, res) {
     try {
@@ -57,6 +90,24 @@ let getByPin = async function (req, res) {
     }
 }
 
+let getStates = async function (req, res) {
+    
+    try {
+        let options = {
+            method: 'get',
+            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
@@ -67,7 +118,7 @@ let getOtp = async function (req, res) {
             url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
             data: blahhh
         }
-
+        
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
@@ -79,7 +130,16 @@ let getOtp = async function (req, res) {
 }
 
 
-module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
+
+module.exports.getDistrictSessions = getDistrictSessions
+
+module.exports.getSortedCities = getSortedCities
+
+module.exports.memeHandler = memeHandler
+
+
 module.exports.getByPin = getByPin
+
 module.exports.getOtp = getOtp
+
+module.exports.getStates = getStates
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
+const CowinController = require("../controllers/cowinController")
 
 
 
@@ -9,14 +9,19 @@ router.get("/test-me", function (req, res) {
 })
 
 
+
+router.get("/getSortedCities", CowinController.getSortedCities)
+
+router.get("/cowin/getByDistrict", CowinController.getDistrictSessions)
+
+router.post("/meme", CowinController.memeHandler)
+
 router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-
+//router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
 
 module.exports = router;
